390970345c18b2717a66c3263dfa8b078d880ecf
Minghua Sun <clairesunstudio@gmail.com>
2017-09-22 22:49:36 -0400

    fetch images from images folder using axios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Masonry from './Masonry'
 import './App.css';
-
+import axios from 'axios'
 
 const Tile = ({src}) => {
   return (
@@ -12,23 +12,48 @@ const Tile = ({src}) => {
   );
 };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-				<div className="masonry-container">
-					<p>ReactJS Responsive/Modular Masonry Grid.</p>
-					<Masonry brakePoints={this.props.brakePoints}>
-						{this.props.images.map((image, id) => {
-							return (
-								<Tile src={image} />
-							)
-						})}
-					</Masonry>
-				</div>
-			</div>
-    );
-  }
-}
+    class App extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          imgId: []
+        };
+      }
+      componentWillMount = () =>  {
+        this.fetchImages()
+      };
+      fetchImages = () =>  {
+        const _this = this
+          axios.get('images')
+            .then(function (response) {
+              const imgId = response.data
+              _this.setState({imgId})
+            })
+          .catch((error) => {console.log(error)})
+      };
+      render() {
+        const {imgId} = this.state
+        let brakePoints = [350, 500, 750];
+        let images = [];
+        for(let i = 0; i< imgId.length; i++){
+        	images.push("images/" + imgId[i]);
+        }
+        return (
+          <div className="container">
+    				<div className="masonry-container">
+    					<p>ReactJS Responsive/Modular Masonry Grid.</p>
+    					<Masonry brakePoints={brakePoints}>
+    						{images.map((image, id) => {
+    							return (
+    								<Tile src={image} />
+    							)
+    						})}
+    					</Masonry>
+    				</div>
+    			</div>
+        );
+      }
+    }
+
 
 export default App;
