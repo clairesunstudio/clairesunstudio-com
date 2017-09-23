0fc456afefaa5a70325c65fac25a07b8183e568d
Minghua Sun <clairesunstudio@gmail.com>
2017-09-22 21:28:52 -0400

    set up responsive masonry grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Masonry from './Masonry'
 import './App.css';
+
+
+const Tile = ({src}) => {
+  return (
+    <div className="tile">
+			<img src={src} />
+		</div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="container">
+				<div className="masonry-container">
+					<p>ReactJS Responsive/Modular Masonry Grid.</p>
+					<Masonry brakePoints={this.props.brakePoints}>
+						{this.props.images.map((image, id) => {
+							return (
+								<Tile src={image} />
+							)
+						})}
+					</Masonry>
+				</div>
+			</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let brakePoints = [350, 500, 750];
+let images = [];
+const imgId = [1011, 883, 1074, 823, 64, 65, 839, 314, 256, 316, 92,643];
+for(let i = 0; i< imgId.length; i++){
+	const ih = 200 + Math.floor(Math.random()*10)*15;
+	images.push("https://unsplash.it/250/" + ih + "?image=" + imgId[i]);
+}
+
+ReactDOM.render(<App images = {images} brakePoints={brakePoints} />, document.getElementById('root'));
 registerServiceWorker();
