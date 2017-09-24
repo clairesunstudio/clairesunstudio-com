59b8d7bb81b6cccff02d2bf9e41bb8805148aed6
Minghua Sun <clairesunstudio@gmail.com>
2017-09-23 20:01:08 -0400

    Add Header nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from './Header'
 import Gallery from './Gallery'
 import Search from './Search'
 import './App.css';
 //import axios from 'axios'
 import * as sampleData from './sampleData.js';
+
 
 var data = sampleData.screens
     class App extends Component {
@@ -29,13 +31,15 @@ var data = sampleData.screens
              });
              this.setState({data})
       };
-
       render() {
         return (
-          <div className="container">
-              <Search onFilter={this.onFilter} />
-              <Gallery data={this.state.data}/>
-    			</div>
+          <div>
+            <Header />
+            <div className="container">
+                <Search onFilter={this.onFilter} />
+                <Gallery data={this.state.data}/>
+            </div>
+          </div>
         );
       }
     }
