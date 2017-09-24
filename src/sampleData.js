05c4439f7ca4983969a361cb12d029b1d16baea8
Minghua Sun <clairesunstudio@gmail.com>
2017-09-24 19:19:04 -0400

    add freshmedia part 1 and part 2 to gallery

--- src/sampleData.js
+++ src/sampleData.js
@@ -5,7 +5,8 @@ module.exports = {
     {'url': 'dese', 'name': 'High School Success Data Story', 'sort': 3, 'key': 3, 'description': 'An interactive data visualization built with D3.js'},
     {'url': 'fashion', 'name': 'fashion', 'sort': 4, 'key': 4, 'description': 'An interactive data visualization built with D3.js'},
     {'url': 'map', 'name': 'map', 'sort': 5, 'key': 5, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'freshmedia', 'name': 'fresh media', 'sort': 6, 'key': 6, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'bnbgirls', 'name': 'bnbgirls', 'sort': 7, 'key': 7, 'description': 'An interactive data visualization built with D3.js'}
+    {'url': 'freshmedia1', 'name': 'fresh media Part I', 'sort': 6, 'key': 6, 'description': 'Sensor Light Installation at the Fresh Media Show'},
+    {'url': 'freshmedia2', 'name': 'fresh media Part II', 'sort': 6, 'key': 7, 'description': 'Sensor Light Installation at the Fresh Media Show'},
+    {'url': 'bnbgirls', 'name': 'bnbgirls', 'sort': 7, 'key': 8, 'description': 'An interactive data visualization built with D3.js'}
    ]
   }
