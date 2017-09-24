ab3738ec2348bebb17bbec5fbb65e3335d048e03
Minghua Sun <clairesunstudio@gmail.com>
2017-09-24 18:08:55 -0400

    add feature image to Project

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './project/Header'
 import Content from './project/Content'
+import Media from './project/Media'
 import axios from 'axios'
 import { Col } from 'react-bootstrap'
 import './Project.css'
@@ -11,6 +12,7 @@ class Project extends React.Component{
     this.state = {
       content: null,
       imgId: [],
+      imgFeature: [],
       projectId: this.props.match.params.projectId
     }
   }
@@ -22,37 +24,39 @@ class Project extends React.Component{
     const projectId = _this.state.projectId
       axios.all([
         axios.get('projects/'+projectId+'/content.json'),
-        axios.get('projects/'+projectId+'/media')
+        axios.get('projects/'+projectId+'/media'),
+        axios.get('projects/'+projectId+'/media/feature')
       ])
-      .then(axios.spread(function(result, media) {
+      .then(axios.spread(function(result, media, feature) {
           const content = result.data
           const imgId = media.data
-          _this.setState({imgId, content})
+          const imgFeature = feature.data
+          _this.setState({imgId, content, imgFeature})
         }))
       .catch((error) => {console.log(error)})
   };
   render(){
-    const {imgId, content, projectId} = this.state
-    console.log(projectId)
+    const {imgId, content, imgFeature, projectId} = this.state
     if( !content ) {
       return <div className="container">Loading...</div>
     }
-    let images = [];
+    let images = [], features = [];
     for(let i = 0; i< imgId.length; i++){
-      images.push("projects/"+projectId+"/media" + imgId[i]);
+      if(imgId[i] != 'feature'){
+        images.push("projects/"+projectId+"/media/" + imgId[i]);
+      }
     }
-    console.log(content)
-    const {title, subtitle, sections} = content
+    for(let i = 0; i< imgFeature.length; i++){
+      features.push("projects/"+projectId+"/media/feature/" + imgFeature[i]);
+    }
+    console.log(images)
+    const {title, subtitle, sections, live_site} = content
     return (
         <div className="wrapper">
           <Header title={title} subtitle={subtitle} live_site={live_site}/>
           <div className="container">
             <Content sections={sections}/>
-            <Col md={8} className="media">
-              <Col md={12}><img src={`projects/freshmedia/mediadino.gif`} width="300" height="200"/></Col>
-              <Col md={6}><img src={`projects/freshmedia/${images[1]}`} width="300" height="200"/></Col>
-              <Col md={6}><img src={`projects/freshmedia/${images[1]}`} width="300" height="200"/></Col>
-            </Col>
+            <Media images={images} features={features}/>
           </div>
         </div>
     )
--- src/project/Media.js
+++ src/project/Media.js
@@ -3,13 +3,17 @@ import { Col } from 'react-bootstrap'
 import './Media.css'
 
 
-const Media = ({images}) => {
+const Media = ({images, features}) => {
   console.log(images)
   const imageList = images.map((image, i) => (
     <Col md={6} key={`image_+${i}`}><img src={images[i]}/></Col>
   ))
+  const featureList = features.map((feature, i) => (
+    <Col md={12} key={`feature_+${i}`}><img src={features[i]}/></Col>
+  ))
     return (
       <Col md={8} className="media">
+        {featureList}
         {imageList}
       </Col>
     );
