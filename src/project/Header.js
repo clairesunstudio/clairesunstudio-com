37e01c4a172abc12f6bd471f49082f06caff40e3
Minghua Sun <clairesunstudio@gmail.com>
2017-09-24 18:09:38 -0400

    add live site button to project header

--- src/project/Header.js
+++ src/project/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Col } from 'react-bootstrap'
+import { Col, Button } from 'react-bootstrap'
 import './Header.css'
 
-const Header = ({title,subtitle}) => {
-  console.log(subtitle)
+const Header = ({title,subtitle, live_site}) => {
+  console.log(live_site)
+
     return (
       <header className="header-bar">
         <div className="container">
@@ -11,6 +12,7 @@ const Header = ({title,subtitle}) => {
             <Col md={8} className="content">
               <h1>{title}</h1>
               <h3>{subtitle}</h3>
+              <Button href={live_site}>Visit Live Site</Button>
             </Col>
           </div>
         </div>
