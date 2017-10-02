6af3c1a44e3f0d4dc276be183587464664d2d10d
Minghua Sun <clairesunstudio@gmail.com>
2017-10-02 01:10:39 -0400

    attempt to fix ios hover state

--- src/Card.js
+++ src/Card.js
@@ -11,7 +11,7 @@ export default class Card extends React.Component {
     const itemStyle = {
       backgroundImage: `url('images/${url}.jpg')`
     };
-    return <div style={itemStyle} className="gridItem">
+    return <div onClick="void(0)" style={itemStyle} className="gridItem">
               <div className="gridItem__cover">
                 <div className="gridItem__cover-content">
                   <h3>{name}</h3>
