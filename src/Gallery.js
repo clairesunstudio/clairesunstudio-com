2281e31983df514b0ac08a4777994db9b8ae2381
Minghua Sun <clairesunstudio@gmail.com>
2017-10-03 00:08:41 -0400

    add category filter

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,30 +1,95 @@
 import React from 'react';
-import createAbsoluteGrid from 'react-absolute-grid';
 import Card from './Card';
 import './Gallery.css'
 
-// Pass your display component to create a new grid
-const AbsoluteGrid = createAbsoluteGrid(Card);
+var FilterPanel = React.createClass({
 
-class Gallery extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	render: function(){
+		return (
+			<a onClick={this.props.onClick}>{this.props.category}</a>
 
-  render(){
-    const {data} = this.props
-    return (
-      <div className="gallery">
-        <AbsoluteGrid items={data}
-                               dragEnabled={false}
-                               responsive={true}
-                               verticalMargin={42}
-                               itemWidth={400}
-                               itemHeight={200}/>
-      </div>
+		)
+	}
 
-    )
-  }
-}
+});
 
-export default Gallery
+var PhotoGallery = React.createClass({
+
+	getInitialState: function() {
+                return {
+                    displayedCategories: this.props.data,
+                    active: false
+                };
+            },
+     toggleActive: function() {
+        this.setState({
+            active: !this.state.active
+        });
+    },
+
+	selectCategory(element){
+
+		  var categoryName = element;
+    console.log('category is: '+ categoryName);
+                var displayedCategories = this.props.data.filter(function(el) {
+
+                    var searchValue = el.category;
+                    return searchValue.indexOf(categoryName) !== -1;
+                    console.log(searchValue);
+                });
+
+                this.setState({
+                    displayedCategories
+
+                });
+
+	},
+	resetFilter: function(element){
+		this.setState({
+                    displayedCategories: this.props.data
+
+                });
+	},
+
+
+
+	render: function(){
+		var buttonClass = this.state.active ? 'active' : '';
+		var categoryToSelect = this.selectCategory;
+		var getUniqueCategories=[];
+		this.props.data.forEach(function(el){
+			if(getUniqueCategories.indexOf(el.category) === -1 ) getUniqueCategories.push(el.category);
+		})
+
+		return (
+			<div className="overlay-photogallery">
+				<div className="filter-panel">
+					{
+						getUniqueCategories.map(function(el,i){
+							var boundClick = categoryToSelect.bind(null,el);
+							return <FilterPanel onClick={boundClick} category={el} key={i} />
+						})
+
+					}
+					<a className="resetBtn" onClick={this.resetFilter}> Reset Filter </a>
+				</div>
+				<div className="gallery">
+					{
+
+						this.state.displayedCategories.map(function(el){
+									return <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3"><Card item={el}/></div>
+								})
+
+					}
+				</div>
+			</div>
+			);
+
+
+	}
+
+
+
+});
+
+export default PhotoGallery
--- src/sampleData.js
+++ src/sampleData.js
@@ -1,13 +1,13 @@
 module.exports = {
   screens: [
-    {'url': 'billionaire', 'name': 'billionaire', 'sort': 1, 'key': 1, 'description': 'An interactive data visualization built with D3.js' },
-    {'url': 'chapter55', 'name': 'chapter55', 'sort': 2, 'key': 2, 'description': 'An interative data story built for Massachusetts Department of Public Health on the opioid epidemic'},
-    {'url': 'dese', 'name': 'High School Success Data Story', 'sort': 3, 'key': 3, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'fashion', 'name': 'fashion', 'sort': 4, 'key': 4, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'map', 'name': 'map', 'sort': 5, 'key': 5, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'freshmedia1', 'name': 'fresh media Part I', 'sort': 6, 'key': 6, 'description': 'Sensor Light Installation at the Fresh Media Show'},
-    {'url': 'freshmedia2', 'name': 'fresh media Part II', 'sort': 6, 'key': 7, 'description': 'Sensor Light Installation at the Fresh Media Show'},
-    {'url': 'bnbgirls', 'name': 'bnbgirls', 'sort': 7, 'key': 8, 'description': 'An interactive data visualization built with D3.js'},
-    {'url': 'facebookfan', 'name': 'The Facebook Fan', 'sort': 7, 'key': 9, 'description': 'An interactive data visualization built with D3.js'}
+    {'url': 'billionaire', 'name': 'billionaire', 'sort': 1, 'key': 1, 'description': 'An interactive data visualization built with D3.js', 'category': 'react'},
+    {'url': 'chapter55', 'name': 'chapter55', 'sort': 2, 'key': 2, 'description': 'An interative data story built for Massachusetts Department of Public Health on the opioid epidemic', 'category': 'design'},
+    {'url': 'dese', 'name': 'High School Success Data Story', 'sort': 3, 'key': 3, 'description': 'An interactive data visualization built with D3.js', 'category': 'design'},
+    {'url': 'fashion', 'name': 'fashion', 'sort': 4, 'key': 4, 'description': 'An interactive data visualization built with D3.js', 'category': 'design'},
+    {'url': 'map', 'name': 'map', 'sort': 5, 'key': 5, 'description': 'An interactive data visualization built with D3.js', 'category': 'design'},
+    {'url': 'freshmedia1', 'name': 'fresh media Part I', 'sort': 6, 'key': 6, 'description': 'Sensor Light Installation at the Fresh Media Show', 'category': 'design'},
+    {'url': 'freshmedia2', 'name': 'fresh media Part II', 'sort': 6, 'key': 7, 'description': 'Sensor Light Installation at the Fresh Media Show', 'category': 'design'},
+    {'url': 'bnbgirls', 'name': 'bnbgirls', 'sort': 7, 'key': 8, 'description': 'An interactive data visualization built with D3.js', 'category': 'design'},
+    {'url': 'facebookfan', 'name': 'The Facebook Fan', 'sort': 7, 'key': 9, 'description': 'An interactive data visualization built with D3.js', 'category': 'react'}
    ]
   }
