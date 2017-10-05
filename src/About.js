5b803db01666769f71019aeab03e8e61368f48c6
Minghua Sun <clairesunstudio@gmail.com>
2017-10-05 01:02:48 -0400

    add react-grid-layout

--- src/About.js
+++ src/About.js
@@ -1,10 +1,104 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import {Responsive, WidthProvider} from 'react-grid-layout';
+import './About.css'
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-export default class About extends React.Component {
+class ShowcaseLayout extends React.Component {
+
+  static propTypes = {
+    onLayoutChange: PropTypes.func.isRequired
+  };
+
+  static defaultProps = {
+    className: "layout",
+    rowHeight: 30,
+    onLayoutChange: function() {},
+    cols: {lg: 12, md: 9, sm: 6, xs: 6, xxs: 3},
+    initialLayout: generateLayout()
+  };
+
+  state = {
+    currentBreakpoint: 'lg',
+    mounted: false,
+    layouts: {lg: this.props.initialLayout},
+  };
+
+  componentDidMount() {
+    this.setState({mounted: true});
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={l.static ? 'static' : ''}>
+          {l.static ?
+            <span className="text">Static - {i}</span>
+            : <span className="text">{i}</span>
+          }
+        </div>);
+    });
+  }
+
+  onBreakpointChange = (breakpoint) => {
+    this.setState({
+      currentBreakpoint: breakpoint
+    });
+  };
+
+  onLayoutChange = (layout, layouts) => {
+    this.props.onLayoutChange(layout, layouts);
+  };
+
+  onNewLayout = () => {
+    this.setState({
+      layouts: {lg: generateLayout()}
+    });
+  };
 
   render() {
-    return <div>
-                About
-            </div>
+    return (
+      <div>
+        <div>Current Breakpoint: {this.state.currentBreakpoint} ({this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}>
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
   }
+}
+
+export default ShowcaseLayout;
+
+function generateLayout() {
+  return _.map(_.range(0, 12), function (item, i) {
+    var x, y, w, h;
+    x = i * 3 % 12;
+    y = Math.floor(i / 4) * 5;
+    w = 3;
+    h = 5;
+
+    console.log(i + ':' + x + ',' + y)
+
+    return {
+      x: x,
+      y: y,
+      w: w,
+      h: h,
+      i: i.toString(),
+      static: true
+    };
+  });
 }
