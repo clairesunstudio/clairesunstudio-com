a4a4bff00bdf40d58a5ad63dbeb8addc413db5cb
Minghua Sun <clairesunstudio@gmail.com>
2017-10-09 22:32:47 -0400

    add projects

--- src/Data/projectData.js
+++ src/Data/projectData.js
@@ -1,192 +1,227 @@
 module.exports =
 [{"type":"directory","name": ".","contents":[
-{"type":"directory","name":"billionaire","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"billionaire.jpg"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"bnbcards","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"bnbgirls_card1.jpg"},
-    {"type":"file","name":"bnbgirls_card2.jpg"},
-    {"type":"file","name":"bnbgirls_card3.jpg"},
-    {"type":"file","name":"bnbgirls_card4.jpg"},
-    {"type":"file","name":"write_event1.jpg"},
-    {"type":"file","name":"write_event2.jpg"},
-    {"type":"file","name":"write_event3.jpg"},
-    {"type":"file","name":"write_event4.jpg"}
-  ]}
-]},
-{"type":"directory","name":"bnbgirls","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"storyboard.jpg"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"chapter55","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"MA-vs-USA.png"},
-    {"type":"file","name":"bsas-percentage.png"},
-    {"type":"file","name":"death-percentage-age.png"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"facebook_bg_851x315.png"}
+    {"type":"directory","name":"BGPS","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"BGPS_4.png"},
+        {"type":"file","name":"BGPS_5.png"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"BGPS.jpg"}
+        ]}
+      ]}
     ]},
-    {"type":"file","name":"prescription-decline.gif"}
-  ]}
-]},
-{"type":"directory","name":"dese","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"dese_district.png"},
-      {"type":"file","name":"dese_grade11.png"},
-      {"type":"file","name":"dese_grade9.png"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"draw","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"draw.jpg"},
-    {"type":"file","name":"pattern1.jpg"},
-    {"type":"file","name":"pattern2.jpg"},
-    {"type":"file","name":"pattern3.jpg"}
-  ]}
-]},
-{"type":"directory","name":"facebookfan","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"video","contents":[
-      {"type":"file","name":"FacebookFan.mp4"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"fashion","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"fashion_ui.png"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"fashion_modal-ui.png"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"freshmedia1","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"arduino_fireflies.jpg"},
-      {"type":"file","name":"fireflies-setup.jpg"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"freshmedia2","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"dino_design-01.jpg"},
-    {"type":"file","name":"dino_design-02.jpg"},
-    {"type":"file","name":"dino_design-03.jpg"},
-    {"type":"file","name":"dino_design-04.jpg"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"dino.gif"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"jewelry","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"facebook-results.png"},
-      {"type":"file","name":"jewelry-1.jpg"}
+    {"type":"directory","name":"billionaire","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"billionaire.jpg"}
+        ]}
+      ]}
     ]},
-    {"type":"file","name":"jewelry-2.jpg"},
-    {"type":"file","name":"jewelry.jpg"}
-  ]}
-]},
-{"type":"directory","name":"map","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"map_design.jpg"}
+    {"type":"directory","name":"bnbcards","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"bnbgirls_card1.jpg"},
+        {"type":"file","name":"bnbgirls_card2.jpg"},
+        {"type":"file","name":"bnbgirls_card3.jpg"},
+        {"type":"file","name":"bnbgirls_card4.jpg"},
+        {"type":"file","name":"write_event1.jpg"},
+        {"type":"file","name":"write_event2.jpg"},
+        {"type":"file","name":"write_event3.jpg"},
+        {"type":"file","name":"write_event4.jpg"}
+      ]}
     ]},
-    {"type":"file","name":"map.jpg"},
-    {"type":"file","name":"map_cover.jpg"},
-    {"type":"file","name":"map_inner.jpg"},
-    {"type":"file","name":"map_open.jpg"},
-    {"type":"file","name":"map_user0.jpg"},
-    {"type":"file","name":"map_user1.jpg"},
-    {"type":"file","name":"map_user2.jpg"}
-  ]}
-]},
-{"type":"directory","name":"massanimalfund","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"Billboard1-Line33F-2017-1400_400.jpg"},
-      {"type":"file","name":"Billboard3-Line32F-2017_1400x400.jpg"},
-      {"type":"file","name":"Billboard4-Line32F-2017_1400x400.jpg"}
+    {"type":"directory","name":"bnbgirls","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"storyboard.jpg"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"chapter55","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"MA-vs-USA.png"},
+        {"type":"file","name":"bsas-percentage.png"},
+        {"type":"file","name":"death-percentage-age.png"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"facebook_bg_851x315.png"}
+        ]},
+        {"type":"file","name":"prescription-decline.gif"}
+      ]}
+    ]},
+    {"type":"directory","name":"dese","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"dese_district.png"},
+          {"type":"file","name":"dese_grade11.png"},
+          {"type":"file","name":"dese_grade9.png"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"draw","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"draw.jpg"},
+        {"type":"file","name":"pattern1.jpg"},
+        {"type":"file","name":"pattern2.jpg"},
+        {"type":"file","name":"pattern3.jpg"}
+      ]}
+    ]},
+    {"type":"directory","name":"facebookfan","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"useless_machine.JPG"}
+        ]},
+        {"type":"directory","name":"video","contents":[
+          {"type":"file","name":"FacebookFan.mp4"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"fashion","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"fashion_ui.png"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"fashion_modal-ui.png"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"freshmedia1","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"arduino_fireflies.jpg"},
+          {"type":"file","name":"fireflies-setup.jpg"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"freshmedia2","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"dino_design-01.jpg"},
+        {"type":"file","name":"dino_design-02.jpg"},
+        {"type":"file","name":"dino_design-03.jpg"},
+        {"type":"file","name":"dino_design-04.jpg"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"dino.gif"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"jewelry","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"facebook-results.png"},
+          {"type":"file","name":"jewelry-1.jpg"}
+        ]},
+        {"type":"file","name":"jewelry-2.jpg"},
+        {"type":"file","name":"jewelry.jpg"}
+      ]}
+    ]},
+    {"type":"directory","name":"map","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"map_design.jpg"}
+        ]},
+        {"type":"file","name":"map.jpg"},
+        {"type":"file","name":"map_cover.jpg"},
+        {"type":"file","name":"map_inner.jpg"},
+        {"type":"file","name":"map_open.jpg"},
+        {"type":"file","name":"map_user0.jpg"},
+        {"type":"file","name":"map_user1.jpg"},
+        {"type":"file","name":"map_user2.jpg"}
+      ]}
+    ]},
+    {"type":"directory","name":"massanimalfund","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"Billboard1-Line33F-2017-1400_400.jpg"},
+          {"type":"file","name":"Billboard3-Line32F-2017_1400x400.jpg"},
+          {"type":"file","name":"Billboard4-Line32F-2017_1400x400.jpg"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"mdd","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"cleaner facebook.jpg"},
+        {"type":"file","name":"delislicer facebook.jpg"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"facebook-results.png"}
+        ]},
+        {"type":"file","name":"fryolator facebook.jpg"},
+        {"type":"file","name":"ladder facebook.jpg"},
+        {"type":"file","name":"register facebook.jpg"},
+        {"type":"file","name":"wheelchair facebook.jpg"}
+      ]}
+    ]},
+    {"type":"directory","name":"readyMA","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"buildKit.jpg"},
+        {"type":"file","name":"familyPlan.jpg"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"Know-Your-Zone-MEMA.jpg"}
+        ]},
+        {"type":"file","name":"hurricaneSeason.jpg"},
+        {"type":"file","name":"knowYourZone.jpg"},
+        {"type":"file","name":"stayInformed.jpg"}
+      ]}
+    ]},
+    {"type":"directory","name":"selfie","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"selfie.jpg"},
+        {"type":"file","name":"selfie_2.png"},
+        {"type":"directory","name":"video","contents":[
+          {"type":"file","name":"sort-video.mp4"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"ssss","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"SSSS-bag.png"},
+        {"type":"file","name":"SSSS-building.png"},
+        {"type":"file","name":"SSSS-car.png"},
+        {"type":"file","name":"SSSS-crowd.png"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"SSSS-infographic.png"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"wefilm","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"SSSS-bag.png"},
+        {"type":"file","name":"SSSS-building.png"},
+        {"type":"file","name":"SSSS-car.png"},
+        {"type":"file","name":"SSSS-crowd.png"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"SSSS-infographic.png"}
+        ]}
+      ]}
+    ]},
+    {"type":"directory","name":"yes","contents":[
+      {"type":"file","name":"content.json"},
+      {"type":"directory","name":"media","contents":[
+        {"type":"file","name":"cleaner facebook.jpg"},
+        {"type":"file","name":"delislicer facebook.jpg"},
+        {"type":"directory","name":"feature","contents":[
+          {"type":"file","name":"facebook-results.png"}
+        ]},
+        {"type":"file","name":"fryolator facebook.jpg"},
+        {"type":"file","name":"ladder facebook.jpg"},
+        {"type":"file","name":"register facebook.jpg"},
+        {"type":"file","name":"wheelchair facebook.jpg"}
+      ]}
     ]}
-  ]}
-]},
-{"type":"directory","name":"mdd","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"cleaner facebook.jpg"},
-    {"type":"file","name":"delislicer facebook.jpg"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"facebook-results.png"}
-    ]},
-    {"type":"file","name":"fryolator facebook.jpg"},
-    {"type":"file","name":"ladder facebook.jpg"},
-    {"type":"file","name":"register facebook.jpg"},
-    {"type":"file","name":"wheelchair facebook.jpg"}
-  ]}
-]},
-{"type":"directory","name":"readyMA","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"buildKit.jpg"},
-    {"type":"file","name":"familyPlan.jpg"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"Know-Your-Zone-MEMA.jpg"}
-    ]},
-    {"type":"file","name":"hurricaneSeason.jpg"},
-    {"type":"file","name":"knowYourZone.jpg"},
-    {"type":"file","name":"stayInformed.jpg"}
-  ]}
-]},
-{"type":"directory","name":"ssss","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"SSSS-bag.png"},
-    {"type":"file","name":"SSSS-building.png"},
-    {"type":"file","name":"SSSS-car.png"},
-    {"type":"file","name":"SSSS-crowd.png"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"SSSS-infographic.png"}
-    ]}
-  ]}
-]},
-{"type":"directory","name":"yes","contents":[
-  {"type":"file","name":"content.json"},
-  {"type":"directory","name":"media","contents":[
-    {"type":"file","name":"cleaner facebook.jpg"},
-    {"type":"file","name":"delislicer facebook.jpg"},
-    {"type":"directory","name":"feature","contents":[
-      {"type":"file","name":"facebook-results.png"}
-    ]},
-    {"type":"file","name":"fryolator facebook.jpg"},
-    {"type":"file","name":"ladder facebook.jpg"},
-    {"type":"file","name":"register facebook.jpg"},
-    {"type":"file","name":"wheelchair facebook.jpg"}
-  ]}
-]}
-]},
-{"type":"report","directories":49,"files":89}
+  ]},
+  {"type":"report","directories":59,"files":104}
 ]
