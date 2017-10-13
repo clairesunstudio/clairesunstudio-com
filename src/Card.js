376129419550acc5119249fa7968dcff8eea8488
Minghua Sun <clairesunstudio@gmail.com>
2017-10-13 00:16:49 -0400

    fix hover on mobile

--- src/Card.js
+++ src/Card.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './Card.css'
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-//import './scripts/fixWidows'
-import touchDevice from './scripts/touchDevice'
+import Modernizr from './scripts/modernizr'
+import $ from 'jquery'
 
 export default class Card extends React.Component {
-
+  onCardClick(){
+    if(Modernizr.touch){
+      $('.gridItem').on('click', function(e){
+        var $card = $(this);
+        console.log(e)
+        $card.addClass('hover').siblings().removeClass('hover');
+      });
+    }
+  }
   render() {
-    touchDevice(window);
+
     const {url, name, description} = this.props.item
     const itemStyle = {
       backgroundImage: `url('images/${url}.jpg')`
     };
-    return <div style={itemStyle} className="gridItem">
+    return <div onClick={this.onCardClick} style={itemStyle} className="gridItem">
               <div className="gridItem__cover">
                 <div className="gridItem__cover-content">
                   <h3>{name}</h3>
