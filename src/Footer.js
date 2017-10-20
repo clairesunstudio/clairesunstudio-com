ee4fecd202548e8e4ccd2cbb7974913e3f31fc9d
Minghua Sun <clairesunstudio@gmail.com>
2017-10-20 00:42:59 -0400

    remove footer position js

--- src/Footer.js
+++ src/Footer.js
@@ -6,20 +6,12 @@ import $ from 'jquery'
 
 class Footer extends React.Component{
   render = () =>  {
-    let h = $('.home_gallery').height();
-    let wh = $(window).height()
-    let pos = h < wh ? 'fixed':'relative'
-    console.log(pos)
-
     const socialIconStyle ={
       width:30,height:30, margin:3
     }
     const socialIconColor = "#fff"
-    const style = {
-      position: pos
-    }
     return (
-      <footer style={style}>
+      <footer>
         <div className="container">
             <Col sm={6}>
                 <p><strong>© 2017 Claire Sun Studio </strong></p>
