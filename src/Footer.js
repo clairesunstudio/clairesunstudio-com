8408b96516ed2fb140f7442a631dd54684c291ed
Minghua Sun <clairesunstudio@gmail.com>
2017-10-19 23:35:46 -0400

    fix footer positioning

--- src/Footer.js
+++ src/Footer.js
@@ -2,16 +2,24 @@ import React from 'react'
 import './Footer.css'
 import SocialMedia from './SocialMedia'
 import { Col } from 'react-bootstrap'
+import $ from 'jquery'
 
 class Footer extends React.Component{
-
   render = () =>  {
+    let h = $('.home_gallery').height();
+    let wh = $(window).height()
+    let pos = h < wh ? 'fixed':'relative'
+    console.log(pos)
+
     const socialIconStyle ={
       width:30,height:30, margin:3
     }
     const socialIconColor = "#fff"
+    const style = {
+      position: pos
+    }
     return (
-      <footer>
+      <footer style={style}>
         <div className="container">
             <Col sm={6}>
                 <p><strong>© 2017 Claire Sun Studio </strong></p>
