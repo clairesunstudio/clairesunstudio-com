167cfcc1399a828cae02fc608be5dd8420d706e6
Minghua Sun <clairesunstudio@gmail.com>
2017-10-23 22:18:07 -0400

    set margin top on About page

--- src/About.js
+++ src/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './About.css'
 
 export default class About extends React.Component {
 
   render() {
     return (
+      <div className="resume_container">
       <iframe src="http://docs.google.com/gview?url=https://clairesunstudio.github.io/img/minghua-resume.pdf&embedded=true"
       style={{width:800, height:1100}} frameborder="0"></iframe>
+      </div>
     )
   }
 }
