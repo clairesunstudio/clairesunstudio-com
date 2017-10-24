527ff753559d3810acda48c9cf3cfab7f5de795a
Minghua Sun <clairesunstudio@gmail.com>
2017-10-24 15:16:59 -0400

    refactor timeline and add content

--- src/resume/SkillCloud.js
+++ src/resume/SkillCloud.js
@@ -2,27 +2,27 @@ import React from 'react'
 import { TagCloud } from "react-tagcloud";
 
 const SkillCloud = ({children}) => {
-  const color = `#263238`
+  const color = `#FF0066`
   const data = [
-    {value: "Atomic Design", count:15, color:{color}},
-    {value: "Rapid Prototyping", count:18, color:{color}},
-    {value: "Data Visualization", count:30, color:{color}},
-    {value: "Front End Development", count:30, color:{color}},
-    {value: "User Research", count:16, color:{color}},
-    {value: "UX Design", count:30, color:{color}},
-    {value: "Installation Art", count:20, color:{color}},
-    {value: "Animation", count:20, color:{color}},
-    {value: "UI Design", count:20, color:{color}},
-    {value: "Branding", count:20, color:{color}},
-    {value: "Marketing Campaign", count:15, color:{color}},
-    {value: "Design Thinking", count:20, color:{color}},
-    {value: "Social Media", count:20, color:{color}},
-    {value: "Storytelling", count:20, color:{color}},
-    {value: "Data Analytics", count:20, color:{color}},
-    {value: "Interaction Design", count:20, color:{color}},
-    {value: "Agile", count:13, color:{color}},
-    {value: "Project Management", count:10, color:{color}},
-    {value: "Accessibility", count:15, color:{color}}
+    {value: "Atomic Design", count:15, color: color},
+    {value: "Rapid Prototyping", count:18, color: color},
+    {value: "Data Visualization", count:30, color: color},
+    {value: "Front End Development", count:30, color: color},
+    {value: "User Research", count:16, color: color},
+    {value: "UX Design", count:30, color: color},
+    {value: "Installation Art", count:20, color: color},
+    {value: "Animation", count:20, color: color},
+    {value: "UI Design", count:20, color: color},
+    {value: "Branding", count:20, color: color},
+    {value: "Marketing Campaign", count:15, color: color},
+    {value: "Design Thinking", count:20, color: color},
+    {value: "Social Media", count:20, color: color},
+    {value: "Storytelling", count:20, color: color},
+    {value: "Data Analytics", count:20, color: color},
+    {value: "Interaction Design", count:20, color: color},
+    {value: "Agile", count:13, color: color},
+    {value: "Project Management", count:10, color: color},
+    {value: "Accessibility", count:15, color: color}
   ];
     return (
       <TagCloud minSize={10}
--- src/resume/Timeline.js
+++ src/resume/Timeline.js
@@ -1,43 +1,33 @@
 import React from 'react'
 import './Timeline.css'
 
-const Timeline = ({children}) => {
+export const TimelineEvent = ({title, address, date, children}) => {
+  const formatDate = (date) => {
+    if (typeof(date)==='string'){
+      return date
+    }else{
+      return <p>{date[0]} <br /> {date[1]}</p>
+      }
+  }
+    return (
+        <div>
+          <div className="timeline_line"></div>
+          <div className="timeline_event">
+          <div className="timeline_event-date">{formatDate(date)}</div>
+            <div className="timeline_event-icon"></div>
+            <div className="timeline_event-content">
+            <h4>{title}</h4>
+            <address>{address}</address>
+            {children}
+            </div>
+          </div>
+        </div>
+    );
+}
+export const Timeline = ({children}) => {
     return (
       <section className="timeline_section">
-        <div className="timeline_line"></div>
-        <div className="timeline_event">
-        <div className="timeline_event-date">May 2015</div>
-          <div className="timeline_event-icon"></div>
-          <div className="timeline_event-content">
-          <h4>Graduate Certificate in Interactive Design and Dynamic Media</h4>
-          <address>MassArt, Boston, MA</address>
-          </div>
-        </div>
-        <div className="timeline_event">
-        <div className="timeline_event-date">May 2015</div>
-          <div className="timeline_event-icon"></div>
-          <div className="timeline_event-content">
-          <h4>Interactive Environment Group</h4>
-          <address>MIT Media Lab, Cambridge, MA</address>
-          </div>
-        </div>
-        <div className="timeline_event">
-        <div className="timeline_event-date">May 2013</div>
-          <div className="timeline_event-icon"></div>
-          <div className="timeline_event-content">
-          <h4>B.S. in Applied Mathematics, B.A. in Graphic Design</h4>
-          <address>New England College, Henniker, NH</address>
-          <ul>
-            <li> UI/UX design, web front end development</li>
-            <li> Analyze and visualize data for Mass.gov social media accounts</li>
-            <li> Lead public facing marketing campaign branding and visual design</li>
-            <li> Direct and mentor design interns</li>
-          </ul>
-          </div>
-        </div>
-
+        {children}
       </section>
     );
 }
-
-export default Timeline
