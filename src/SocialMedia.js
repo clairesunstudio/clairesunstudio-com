e8472b587fbb8e093ada3b6b17340b3e4a8f71d7
Minghua Sun <clairesunstudio@gmail.com>
2018-06-23 13:51:57 -0400

    remove fb and ig links

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -7,9 +7,7 @@ const SocialMedia = ({socialIconStyle,socialIconColor,collapsed}) => {
             <div className={`social-media ${collapsed}`}>
             <SocialIcon style={socialIconStyle} color={socialIconColor} url="https://www.linkedin.com/in/minghuasun/" />
             <SocialIcon style={socialIconStyle} color={socialIconColor} url="http://github.com/clairesunstudio" />
-            <SocialIcon style={socialIconStyle} color={socialIconColor} url="https://www.facebook.com/clairesunstudio" />
             <SocialIcon style={socialIconStyle} color={socialIconColor} url="https://www.twitter.com/iamclairesun" />
-            <SocialIcon style={socialIconStyle} color={socialIconColor} url="https://www.instagram.com/clairelilbear" />
             <SocialIcon style={socialIconStyle} color={socialIconColor} target="_self" network="email" url="mailto:clairesunstudio@gmail.com" />
             </div>
     );
