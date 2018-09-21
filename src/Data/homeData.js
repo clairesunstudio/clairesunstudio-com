881b6ed69df49c797e9e3632d10801eea10a5c59
Minghua Sun <clairesunstudio@gmail.com>
2018-09-20 21:23:52 -0400

    add projects to home

--- src/Data/homeData.js
+++ src/Data/homeData.js
@@ -1,142 +1,117 @@
 module.exports =
 [{
+  "url": "mayflower",
+  "name": "Mayflower Design System",
+  "description": "A commonwealth design system",
+  "category": ["UI", "UX", "design", "web", "development"]
+}, {
+  "url": "Search Mass.gov",
+  "name": "search",
+  "description": "The search application powering Mass.gov",
+  "category": ["UI", "UX", "design", "web", "development"]
+}, {
+  "url": "rideshare",
+  "name": "rideshare",
+  "description": "An interative data story built for Massachusetts Department of Transportation on the rideshare data",
+  "category": ["data visualization", "UI", "UX", "design", "web", "development"]
+}, {
   "url": "chapter55",
   "name": "chapter55",
-  "sort": 1,
-  "key": 1,
   "description": "An interative data story built for Massachusetts Department of Public Health on the opioid epidemic",
   "category": ["data visualization", "UI", "UX", "design", "web", "development"]
 }, {
   "url": "dese",
   "name": "The ABCs of High School Success",
-  "sort": 2,
-  "key": 2,
   "description": "A data story exploring long-term impacts of high school attendance, behavior, and course performance.",
   "category": ["data visualization", "UI", "UX", "design", "web", "development"]
 }, {
   "url": "fashion",
   "name": "The Fashion of Hitchcock’s 7 leading ladies",
-  "sort": 3,
-  "key": 3,
   "description": "Proof of concept prototype for visualizing fashion in classic movies",
   "category": ["data visualization", "prototype"]
 }, {
   "url": "billionaire",
   "name": "An IT Billionaires Map",
-  "sort": 4,
-  "key": 4,
   "description": "An interactive data visualization built with D3.js",
   "category": ["data visualization", "prototype"]
 }, {
   "url": "map",
   "name": "The Massart-MFA Map",
-  "sort": 5,
-  "key": 5,
   "description": "A map design driven by user research and testing",
   "category": ["design", "UX"]
 }, {
   "url": "freshmedia1",
   "name": "fresh media Part I",
-  "sort": 6,
-  "key": 6,
   "description": "Sensor Light Installation at the Fresh Media Show",
   "category": ["sensor", "installation", "art"]
 }, {
   "url": "freshmedia2",
   "name": "fresh media Part II",
-  "sort": 7,
-  "key": 7,
   "description": "Sensor Light Installation at the Fresh Media Show",
   "category": ["sensor", "installation", "art"]
 }, {
   "url": "bnbgirls",
   "name": "The Story of Bright & Beautiful Girls",
-  "sort": 8,
-  "key": 8,
   "description": "Storytelling using parallax animation",
   "category": ["design", "prototype", "web"]
 }, {
   "url": "facebookfan",
   "name": "The Facebook Fan",
-  "sort": 9,
-  "key": 9,
   "description": "Multi sensory experience designed to show media approval",
   "category": ["interative", "installation", "prototype"]
 }, {
   "url": "yes",
   "name": "Young Workers Safety",
-  "sort": 10,
-  "key": 10,
   "description": "A marketing campaign raising awareness of young workers safety and rights",
   "category": ["branding", "design", "marketing"]
 }, {
   "url": "bnbcards",
   "name": "The Story T and PostCard Project",
-  "sort": 11,
-  "key": 11,
   "description": "Design for the non-profit project empowering young embroideress in rural China",
   "category": ["branding", "design", "marketing"]
 }, {
   "url": "ssss",
   "name": "See Something Say Something",
-  "sort": 12,
-  "key": 12,
   "description": "A public safety social media campaign for the 2016 Boston Marathon",
   "category": ["branding", "design", "marketing"]
 }, {
   "url": "massanimalfund",
   "name": "File Taxes Help Animals",
-  "sort": 13,
-  "key": 13,
   "description": "A pro bono marketing campaign for the MassAnimalFund",
   "category": ["design", "marketing"]
 }, {
   "url": "readyMA",
   "name": "Ready MA",
-  "sort": 14,
-  "key": 14,
   "description": "A marketing campaign for MEMA hurricane preparedness month",
   "category": ["design", "marketing"]
 }, {
   "url": "draw",
   "name": "Generative Drawings",
-  "sort": 15,
-  "key": 15,
   "description": "Generative art, patterns creation using Javascript and Processing ",
   "category": ["design", "art", "interative"]
 }, {
   "url": "mdd",
   "name": "My DMI Diary",
-  "sort": 16,
-  "key": 16,
   "description": "Documentation of projects and courses I completed at Dynamic Media Institute",
   "category": ["design", "web", "development"]
 }, {
   "url": "wefilm",
   "name": "Wefilm",
-  "sort": 17,
-  "key": 17,
   "description": "A native app prototype for collaborative filming on mobile",
   "category": ["UI", "UX", "design", "prototype"]
 }, {
   "url": "selfie",
   "name": "Selfie Identifier",
-  "sort": 18,
-  "key": 18,
   "description": "A web app uses uses Face++ API to tag instagram selfies ",
   "category": ["web", "prototype"]
 }, {
   "url": "BGPS",
   "name": "Branding and Logo Design",
-  "sort": 19,
-  "key": 19,
   "description": "Logo design for Boston Group for Psychoanalytic Studies",
   "category": ["branding", "design"]
 }, {
   "url": "jewelry",
   "name": "Jewelry",
-  "sort": 20,
-  "key": 20,
   "description": "Triptych (oil, newsprint on wood panels )",
   "category": ["art"]
 }]
