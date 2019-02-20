356f96d9ffea6f419a3538d164742c557b1acfa4
Minghua Sun <clairesunstudio@gmail.com>
2019-02-20 15:17:03 -0500

    refactor

--- src/project/Media.js
+++ src/project/Media.js
@@ -4,6 +4,14 @@ import { DefaultPlayer as Video } from 'react-html5video';
 import 'react-html5video/dist/styles.css';
 import './Media.css'
 
+const VideoPlayer = ({ video }) => (
+  <Col md={12}>
+    <Video controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}>
+        <source src={video} type="video/mp4" />
+    </Video>
+  </Col>
+)
+
 const Media = ({images, features, videos}) => {
   const imageList = images.map((image, i) => (
     <Col md={6} key={`image_+${i}`}><img src={images[i]}/></Col>
@@ -12,17 +20,9 @@ const Media = ({images, features, videos}) => {
     <Col md={12} key={`feature_+${i}`}><img src={features[i]}/></Col>
   ))
 
-  const VideoPlayer = () => (
-    <Col md={12}>
-      <Video controls={['PlayPause', 'Seek', 'Time', 'Volume', 'Fullscreen']}>
-          <source src={videos[0]} type="video/mp4" />
-      </Video>
-    </Col>
-  )
-
     return (
       <Col md={8} className="media">
-        {videos.length>0 && <VideoPlayer/>}
+        {videos.length>0 && <VideoPlayer video={videos[0]} />}
         {featureList}
         {imageList}
       </Col>
